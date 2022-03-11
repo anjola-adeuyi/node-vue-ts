510ae7e88a42aa3f0d7b754127614ebc94fe5dc1
Anjola Adeuyi <57623705+anjola-adeuyi@users.noreply.github.com>
2022-03-11 22:52:07 +0100

    Express initial setup

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-console.log('Hello Jola')
+import express, { Request, Response } from 'express'
+import cors from 'cors'
+
+const app = express();
+
+app.use(express.json());
+app.use(cors({
+  origin: ["http://localhost:3000"]
+}));
+
+app.get('/', (req: Request, res: Response)=> {
+  res.send("Hello World")
+});
+
+const port = 8000;
+
+app.listen(port, () => {
+  console.log(`listening to port ${port}`)
+})
