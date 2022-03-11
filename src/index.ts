51824e4b961bc5fc871317e17a819c5acaa89d8c
Anjola Adeuyi <57623705+anjola-adeuyi@users.noreply.github.com>
2022-03-12 00:16:38 +0100

    typeOrm and mysql

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,21 @@
 import express, { Request, Response } from 'express'
 import cors from 'cors'
 import { routes } from './routes';
+import { createConnection } from 'typeorm';
 
-const app = express();
+createConnection().then( connection => {
+  const app = express();
 
-app.use(express.json());
-app.use(cors({
-  origin: ["http://localhost:3000"]
-}));
-
-routes(app);
-
-const port = 8000;
-
-app.listen(port, () => {
-  console.log(`listening to port ${port}`)
+  app.use(express.json());
+  app.use(cors({
+    origin: ["http://localhost:3000"]
+  }));
+  
+  routes(app);
+  
+  const port = 8000;
+  
+  app.listen(port, () => {
+    console.log(`listening to port ${port}`)
+  })
 })
