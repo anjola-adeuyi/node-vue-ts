59df468849488bffed53f326fa8479299f37c782
Anjola Adeuyi <57623705+anjola-adeuyi@users.noreply.github.com>
2022-03-11 23:14:37 +0100

    validation

--- src/controller/auth.controller.ts
+++ src/controller/auth.controller.ts
@@ -1,7 +1,22 @@
 import { Request, Response } from "express";
+import { RegisterValidation } from "../validation/register.validation";
 
 const Register = (req: Request, res: Response) => {
-  res.send(req.body)
+  const { body } = req;
+
+  const {error} = RegisterValidation.validate(body);
+
+  if (error) {
+    return res.status(400).send(error.details);
+  }
+
+  if (body && body.password !== body.password_confirm) {
+    res.status(400).send({
+      message: "passwords do not match"
+    })
+  }
+
+  res.send(body)
 }
 
 export default Register
