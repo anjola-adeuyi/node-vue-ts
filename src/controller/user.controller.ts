b2c83823bb0d722fc8b6f75f28a7e74c0b522a29
Anjola Adeuyi <57623705+anjola-adeuyi@users.noreply.github.com>
2022-03-12 04:18:00 +0100

    roles

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -7,7 +7,9 @@ import { User } from "../entity/user.entity";
 export const Users = async (req: Request, res: Response) => {
   const respository = getManager().getRepository(User);
 
-  const users = await respository.find();
+  const users = await respository.find({
+    relations: ['role']
+  });
 
   res.send(users.map(eachUser => {
     const {password, ...data} = eachUser;
@@ -24,7 +26,10 @@ export const CreatUser = async (req: Request, res: Response) => {
 
   const {password, ...user} = await repository.save({
     ...body,
-    password: hashedPassword
+    password: hashedPassword,
+    role: {
+      id: role_id
+    }
   });
 
   res.status(201).send(user);
@@ -33,7 +38,9 @@ export const CreatUser = async (req: Request, res: Response) => {
 export const GetUser = async (req: Request, res: Response) => {
   const repository = getManager().getRepository(User);
 
-  const {password, ...user} = await repository.findOne(req.params.id);
+  const {password, ...user} = await repository.findOne(req.params.id, {
+    relations: ['role']
+  });
 
   res.send(user);
 }
@@ -43,9 +50,16 @@ export const UpdateUser = async (req: Request, res: Response) => {
 
   const repository = getManager().getRepository(User);
 
-  await repository.update(req.params.id, body);
+  await repository.update(req.params.id, {
+    ...body,
+    role: {
+      id: role_id
+    }
+  });
 
-  const {password, ...user} = await repository.findOne(req.params.id);
+  const {password, ...user} = await repository.findOne(req.params.id, {
+    relations: ['role']
+  });
 
   res.status(202).send(user);
 }
