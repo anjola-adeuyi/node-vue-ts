1ea3970f05d8928a9e8fdb8b9a1f3f34bc294f70
Anjola Adeuyi <57623705+anjola-adeuyi@users.noreply.github.com>
2022-03-12 01:46:54 +0100

    login authenticated API

--- src/controller/auth.controller.ts
+++ src/controller/auth.controller.ts
@@ -4,7 +4,7 @@ import bcryptjs from 'bcryptjs';
 import { User } from "../entity/user.entity";
 import { RegisterValidation } from "../validation/register.validation";
 
-const Register = async (req: Request, res: Response) => {
+export const Register = async (req: Request, res: Response) => {
   const { body } = req;
 
   const {error} = RegisterValidation.validate(body);
@@ -31,4 +31,27 @@ const Register = async (req: Request, res: Response) => {
   res.send(user)
 }
 
-export default Register
+export const Login = async (req: Request, res: Response) => {
+  const { body } = req;
+
+  const repository = getManager().getRepository(User);
+
+  const user = await repository.findOne({email: body.email})
+
+  if (!user) {
+    return res.status(404).send({
+      // message: "user not found"
+      messsage: "invalid credentials"
+    })
+  }
+
+  if (!await bcryptjs.compare(body.password, user.password)) {
+    return res.status(400).send({
+      messsage: "invalid credentials"
+    })
+  }
+
+  const {password, ...data} = user;
+
+  res.send(data);
+}
